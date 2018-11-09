dc9802d4bbfc6eeabb3d9609204b926209d784cb
seanjoesting <43583533+seanjoesting@users.noreply.github.com>
2018-11-09 12:14:28 -0500

    Update urls.py

--- sweetdart/urls.py
+++ sweetdart/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('searchbar.urls', namespace = 'searchbar')),
-    path('accounts/', include('accounts.urls', namespace= 'accounts'))
 ]
